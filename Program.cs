0a908aeea8153ed6c94f6f52a62989ff9f354dd2
navabhi22 <navabhi22@gmail.com>
2022-02-20 18:21:18 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -8,16 +8,7 @@ namespace LineComparisonProgram
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
 
-            if (CheckIfLineAreEqual.EqualityOfLine() == true)
-            {
-                Console.WriteLine("Lines are equal");
-            }
-            else 
-            {
-                Console.WriteLine("Lines are not equal");
-            }
-
-
+            ToCompareTheLines.ToCompareLines();
         }
     }
 }
