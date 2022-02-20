6771ce4e01314b7333797286034d5c0132555e50
navabhi22 <navabhi22@gmail.com>
2022-02-20 17:37:39 +0530

    Added class to Calculate Line Length

---
+++ CreatingLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProgram
+{
+    public class CreatingLine
+    {
+        public static void CreateLine()
+        {
+            Random random = new Random();
+            int x1 = random.Next(10);
+            int y1 = random.Next(10);
+
+            int x2 = random.Next(10);
+            int y2 = random.Next(10);
+
+            if (((x2 - x1) + (y2 - y1)) > 0)
+            {
+                double LineLength = Math.Sqrt((x2 - x1) + (y2 - y1));
+                Console.WriteLine("The length of line is  " + LineLength);
+
+            }
+            else
+            {
+                Console.WriteLine("try again coordinate were invalid");
+            }
+        }
+    }
+}
