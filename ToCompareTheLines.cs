38ed9b89fda2c5f2dbb1972be0a88a829a5db8b2
navabhi22 <navabhi22@gmail.com>
2022-02-20 18:20:20 +0530

    Added class to Compare two lines

---
+++ ToCompareTheLines.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProgram
+{
+    public class ToCompareTheLines
+    {
+        public static void ToCompareLines()
+        {
+            double Line1 = CreatingLine.CreateLine();
+            double Line2 = CreatingLine.CreateLine();
+            
+            if (Line1 > Line2)
+            {
+                Console.WriteLine("Line1 is greater than Line2");
+            }
+            else if (Line1 < Line2)
+            {
+                Console.WriteLine("Line2 is greater than Line1 ");
+            }
+            else
+            {
+                Console.WriteLine("Lines are equal");
+            }
+        }
+    }
+}
